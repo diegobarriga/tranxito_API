677edbcea46e10e35cc4e6296f2697c24b935a99
jgmontoya <jgmontoya@uc.cl>
2018-04-30 19:03:59 -0300

    Enable automigrate

--- e2e_eld/server/boot/01_automigrate.js
+++ e2e_eld/server/boot/01_automigrate.js
@@ -6,8 +6,4 @@ module.exports = function(app) {
   app.dataSources.postgresDB.automigrate();
   app.dataSources.mongoDB.automigrate();
 
-  app.dataSources.postgresDB.automigrate(['ACL'], function(er) {
-    if (er) throw er;
-    console.log('Loopback table ACL created' );
-  });
 }
