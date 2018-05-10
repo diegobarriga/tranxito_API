2f2610e8c9ec1a1069c73b55f78ff5a31c5bb0ba
jedelacarrera <jedelacarrera@uc.cl>
2018-05-10 11:23:02 -0300

    the route /MotorCarriers/:id/people/ returns only the non archived people from the motorCarrier with that id

--- common/models/motor-carrier.js
+++ common/models/motor-carrier.js
@@ -11,4 +11,16 @@ module.exports = function(Motorcarrier) {
   Motorcarrier.validatesNumericalityOf('USDOT_number', 'multiday_basis_used', {int: true});
   Motorcarrier.validatesInclusionOf('multiday_basis_used', {in: [7, 8]});
   Motorcarrier.validate('USDOT_number', usdot_validator, {message: 'USDOT number not in range 0 - 999,999,999'});
+
+	Motorcarrier.getPeople = function(id, cb) {
+		Motorcarrier.app.models.Person.find({where: {motorCarrierId: id, account_status: true}}, function(err, data) {
+			cb(err, data);
+		})
+	}
+
+	Motorcarrier.remoteMethod('getPeople', {
+    accepts: {arg: 'id', type: 'string'},
+    returns: {arg: 'data', type: 'string', root: true},
+    http: {path: '/:id/people', verb: 'get'}
+	});
 };
