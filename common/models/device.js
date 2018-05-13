ac7fab357c2bc25d4fd44d2e0f15e4b39bdfe291
jgmontoya <jgmontoya@uc.cl>
2018-05-13 19:23:31 -0400

    Add endpoint to update de configuration script of a device. Add endpoint to label the configuration script of a certain device as valid.

--- common/models/device.js
+++ common/models/device.js
@@ -1,5 +1,57 @@
 'use strict';
 
 module.exports = function(Device) {
+  Device.newConfig = function(id, script, cb) {
+    Device.findById(id, function(err, device) {
+      if (err) {
+        return cb(err);
+      }
+      if (!device) {
+        err = Error('Device not found');
+        err.statusCode = '404';
+        cb(err, 'Device not found');
+      } else {
+        device.configuration_script = script;
+        device.configuration_status = false;
+        device.save();
+        cb(null, 'Configuration Script set correctly');
+      }
+    });
+  };
 
+  Device.remoteMethod(
+    'newConfig',
+    {
+      accepts: [
+        {arg: 'id', type: 'number', required: true},
+        {arg: 'script', type: 'string', required: true},
+      ],
+      http: {path: '/:id/new_config', verb: 'post'},
+      returns: {arg: 'message', type: 'string'},
+    });
+
+  Device.validConfig = function(id, cb) {
+    Device.findById(id, function(err, device) {
+      if (err) {
+        return cb(err);
+      }
+      if (!device) {
+        err = Error('Device not found');
+        err.statusCode = '404';
+        cb(err, 'Device not found');
+      } else {
+        device.configuration_status = true;
+        device.save();
+        cb(null, 'Configuration set as valid');
+      }
+    });
+  };
+
+  Device.remoteMethod(
+    'validConfig',
+    {
+      accepts: {arg: 'id', type: 'number', required: true},
+      http: {path: '/:id/valid_config', verb: 'get'},
+      returns: {arg: 'message', type: 'string'},
+    });
 };
